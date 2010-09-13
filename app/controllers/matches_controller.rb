3517da30001a9e331d9daa5a1bba993a6ef99e6e
Tony Coconate <me@tonycoconate.com>
2010-09-13 17:19:27 -0500

    Going HTML5... more to come.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -24,7 +24,7 @@ class MatchesController < ApplicationController
     body_hash = { :challenger => challenger.display_name, 
                   :incumbent => incumbent.display_name,
                   :tournament => @tournament.name }
-    Notification.deliver_challenge_email(challenger.email, "<#{challenger.email}>, <#{incumbent.email}>", "A ping-pong challenge has been requested!", body_hash)
+    Notification.deliver_challenge_email(challenger.email, "<#{challenger.email}>, <#{incumbent.email}>", "A ping-pong challenge has been requested!", body_hash) rescue nil
     
     respond_to do |format|
       if @match.save
